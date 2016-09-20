6be2a22f5837c901fbb9c8741298876220907ef3
antonzhelyazkov <antonisimo@gmail.com>
2016-09-20 21:29:50 +0300

    added test.pl

---
+++ test.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+
+print "Hello World\n";
+
+my $firstVariable = shift;
+my $secondVariable = shift;
+
+if (!defined($firstVariable) || !defined($secondVariable)) {
+	print "add variables\n";
+		exit(0);
+		}
+
+		my $result = $firstVariable / $secondVariable;
+
+		print "$result\n";
+
+		print ($firstVariable + $secondVariable + 1 / $secondVariable * 5 ) . "\n";
+
